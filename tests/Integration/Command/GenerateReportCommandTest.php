cba69528dc0f44bf88262ed87a5f0671d6ad66f0
dgburns92 <dgburns92@gmail.com>
2023-11-25 17:07:27 +1100

    fix CI by adding sample test

---
+++ tests/Integration/Command/GenerateReportCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Command;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class GenerateReportCommandTest extends KernelTestCase
+{
+    public function testCiChecksWorks(): void
+    {
+        self::assertTrue(true);
+    }
+}
